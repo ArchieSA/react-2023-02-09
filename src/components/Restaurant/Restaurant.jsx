cf931fe042cfbea701920bde0b354ddc34b5ee56
Abros Nakhatakyan <a.nakhatakyan@shakes.pro>
2023-02-19 01:55:30 +0300

    HW1

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { Menu } from "../Menu/Menu";
+import { Reviews } from '../Reviews/Reviews';
 
-export const Restaurant = () => {
-  return <div>Restaurant</div>;
+export const Restaurant = ({ restaurant }) => {
+  const {name, menu, reviews} = restaurant;
+  const restaurantDishes = menu.map(({ name }) => name).join(", ");
+  const restaurantReviews = reviews.map(({ text }) => text).join(", ");
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <Menu menu={restaurantDishes}/>
+      <Reviews reviews={restaurantReviews}/>
+    </div>
+  );
+
+  // return React.createElement('div', {
+  //   children: [
+  //     React.createElement('h2', {
+  //       children: name
+  //     }),
+  //     React.createElement(Menu, {
+  //       menu: restaurantDishes
+  //     }),
+  //     React.createElement(Reviews, {
+  //       menu: restaurantReviews
+  //     })
+  //   ]
+  // })
 };
