f0acf531beca5ae8a3608378411596e83a91d7b2
AAZheltonogov <aazheltonogov@greenatom.ru>
2023-02-18 10:49:40 +0300

    Added dynamic data output for Menus and Reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-export const Reviews = () => {
-  return <div>Reviews</div>;
+export const Reviews = ({data}) => {
+  const starActive = '★'
+  const getViewRating = (rating) => new Array(rating).fill(starActive).join('')
+
+  const getView = ({text,rating,user }) => `${text}
+  Автор: ${user}
+  Рейтинг: ${getViewRating(rating)}
+  
+  `
+  const reviews = data.map(el => getView(el))
+  return (
+      <div>
+        <h3>Reviews</h3>
+        <div style={{whiteSpace: 'pre-line'}}>{reviews}</div>
+      </div>
+  );
 };
