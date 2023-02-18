3561cd3848bc560842beb0a338f0f9d24d685559
Davtyantar <107843395+Davtyantar@users.noreply.github.com>
2023-02-19 00:54:46 +0300

    d.z. 1

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
-export const Menu = () => {
-  return <div>Menu</div>;
+export const Menu = ({ menuItem }) => {
+  console.log(menuItem, "menuItem");
+  const { id, name, price, ingredients } = menuItem;
+
+  return (
+    <div key={id}>
+      <h3> Название блюда: {name}</h3>
+      <p> Цена блюда: {price}$</p>
+      <p>
+        Состав :
+        {ingredients.map((ingredient) => (
+          <li>{ingredient}</li>
+        ))}
+        <hr />
+      </p>
+    </div>
+  );
 };
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-export const Reviews = () => {
-  return <div>Reviews</div>;
+export const Reviews = ({ review }) => {
+  const { id, user, text, rating } = review;
+  return (
+    <div className="reviews" key={id}>
+      <h5 className="user">{user}</h5>
+      <div className="review-rate">
+        <p>{text}</p>
+        <p className="rate">{rating} &#9733;</p>
+      </div>
+    </div>
+  );
 };
