16cb7d3bf8bfc66b4a2b108640db630990b833f5
Valent1325 <maxmalofeev13@gmail.com>
2023-02-19 14:54:41 +0300

    Реализация компонента Restaurant, Menu и Reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
-export const Reviews = () => {
-  return <div>Reviews</div>;
+export const Reviews = ({ reviews }) => {
+  const restaurantReviews = reviews.map(({ text }) => text).join(" ,");
+  return (
+    <div>
+      <h3>Reviews</h3>
+      <div>{restaurantReviews}</div>
+    </div>
+  );
+  // return React.createElement("div", {
+  //   children: [
+  //     React.createElement("h3", {
+  //       children: "Reviews"
+  //     }),
+  //     React.createElement("div", {
+  //       children: restaurantReviews
+  //     }),
+  //   ]
+  // });
 };
