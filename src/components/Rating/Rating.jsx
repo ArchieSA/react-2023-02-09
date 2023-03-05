6033b259d84cedb7240e4a4c8a9da4cfe7bd113a
Dmitrij Kisel <dkisel@lms365.com>
2023-03-05 22:39:27 +0100

    HM#3: Remove console.log

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -7,22 +7,19 @@ const MAX_RATING = 5;
 export const Rating = ({ value, onChange, size = SIZE.s }) => {
     return (
         <div className={styles.root}>
-            {Array.from(Array(MAX_RATING)).map((star, index) => {
-                console.log('RATING', value)
-                return (
-                    <div
-                        onClick={() => onChange && onChange(index + 1)}
-                        className={classNames(
-                            styles.star,
-                            styles[size],
-                            {
-                                [styles.write]: !!onChange,
-                                [styles.gold]: index + 1 <= value
-                            }
-                        )}
-                    />
-                )
-            })}
+            {Array.from(Array(MAX_RATING)).map((star, index) => (
+                <div
+                    onClick={() => onChange && onChange(index + 1)}
+                    className={classNames(
+                        styles.star,
+                        styles[size],
+                        {
+                            [styles.write]: !!onChange,
+                            [styles.gold]: index + 1 <= value
+                        }
+                    )}
+                />
+            ))}
         </div>
     )
 }
